144a86ba327bd9dd82b923bae0fd9a1e1a079b0d
k-kuwahara <kuwahara@lepra.jp>
2016-09-17 00:34:44 +0900

    interactive events handler

--- src/public/server.js
+++ src/public/server.js
@@ -1,20 +1,29 @@
 "use strict";
-var fs     = require('fs');
-var server = require('http').createServer();
-var config = require('./config');
-
-server.on('request', function(req, res) {
-  var stream = fs.createReadStream('index.html');
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  stream.pipe(res);
-});
-var io = require('socket.io')(server);
-
-io.on('connection', function(socket) {
-  socket.emit('greeting', {message: 'hello'}, function (data) {
-    console.log('result: ' + data);
-  });
-});
-
+var fs     = require('fs'),
+    server = require('http').createServer(handler),
+    config = require('./js/config'),
+    io     = require('socket.io').listen(server);
 server.listen(config.port);
+
+function handler(req, res) {
+   fs.readFile(__dirname + '/html/index.html', function(err, data) {
+      if (err) {
+         res.writeHead(500);
+         return res.end('Error');
+      }
+      res.writeHead(200);
+      res.write(data);
+      res.end();
+   })
+}
+
+io.sockets.on('connection', function(socket) {
+   socket.on('emit_from_client', function(data) {
+      var id = socket.id;
+      console.info(data);
+      //socket.broadcast.emit('emit_from_server', '[' + id + ']: ' + data);
+      io.sockets.emit('emit_from_server', '[' + id + ']: ' + data);
+   });
+});
+
 console.info('server listening...');
