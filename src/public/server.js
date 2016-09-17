6d976e218358326e691dbb8f9a2848f50b704886
k-kuwahara <kuwahara@lepra.jp>
2016-09-18 01:02:15 +0900

    add modal window

--- src/public/server.js
+++ src/public/server.js
@@ -6,15 +6,34 @@ var fs     = require('fs'),
 server.listen(config.port);
 
 function handler(req, res) {
-   fs.readFile(__dirname + '/html/index.html', function(err, data) {
-      if (err) {
-         res.writeHead(500);
-         return res.end('Error');
-      }
-      res.writeHead(200);
-      res.write(data);
-      res.end();
-   })
+   var file_path = __dirname + req.url
+   switch (req.url) {
+      case '/':
+         fs.readFile(file_path + 'html/index.html', function(err, data) {
+            if (err) {
+               res.writeHead(500);
+               return res.end('Error');
+            }
+            res.writeHead(200);
+            res.write(data);
+            res.end();
+         })
+         break
+
+      case '/css/bootstrap.min.css':
+         fs.readFile(file_path, 'utf-8', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/css' })
+            res.end(data, 'utf-8')
+         })
+         break
+
+      case '/js/bootstrap.min.js':
+         fs.readFile(file_path, 'utf-8', function (err, data) {
+             res.writeHead(200, { 'Content-Type': 'text/javascript' })
+             res.end(data, 'utf-8')
+         })
+         break
+   }
 }
 
 io.sockets.on('connection', function(socket) {
@@ -25,5 +44,4 @@ io.sockets.on('connection', function(socket) {
       io.sockets.emit('emit_from_server', '[' + id + ']: ' + data);
    });
 });
-
 console.info('server listening...');
